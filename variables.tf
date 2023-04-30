c46aff66d20fae8bb7c0571d53b9e2cd22e40d75
sergey <server813@mail.ru>
2023-04-30 11:50:03 -0400

    Update 30.04.23

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "access_token" {
+  default = "y0_AgAAAAAFPRhrAATuwQAAAADSlqt_z19rhiHoRiCl-okOylsHI4E-q7A"  
+#  type = string 
+#  sensitive = true
+}
+
+
+variable "image_id" {
+  default = "fd812hcj5cbdt7238g2o"
+}
+
+
+variable "access_folder_id" {    
+  default = "b1gpok0ichaplcklr1ve"
+#  type = string
+#  sensitive = true
+}
+ 
